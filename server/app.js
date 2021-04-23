24e43543dbe6d38a2a58a2d97659ff32447a3205
qnguy <qtn3@njit.edu>
2021-04-22 21:41:08 -0500

    Fix semicolon

--- server/app.js
+++ server/app.js
@@ -10,21 +10,21 @@ app.use(express.static('docs'));
 // create express app
 
 // Setup server port
-const port = process.env.PORT || 5000;
+// const port = process.env.PORT || 5000;
 
 // parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // parse requests of content-type - application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 // define a root route
 
 // Require employee routes
-const citiesRoutes = require('./routes/cities.routes')
+const citiesRoutes = require('./routes/cities.routes');
 
 // using as middleware
-app.use('/api/v1/cities', citiesRoutes)
+app.use('/api/v1/cities', citiesRoutes);
 
 app.set('port', process.env.PORT || 8000);
 app.set('ip', process.env.NODEJS_IP || '127.0.0.1');
