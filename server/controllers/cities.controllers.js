ba5322942d4fe897d92b1217709d55875cc110f2
qnguy <qtn3@njit.edu>
2021-04-22 22:20:32 -0500

    Fixed function expression arrow, and space in loop

--- server/controllers/cities.controllers.js
+++ server/controllers/cities.controllers.js
@@ -9,14 +9,14 @@ exports.findAll = (req, res) => {
     });
 };
 
-exports.create = function(req, res) {
-    const new_employee = new City(req.body);
+exports.create = (req, res) => {
+    const newEmployee = new City(req.body);
 
-    //handles null error
-    if(req.body.constructor === Object && Object.keys(req.body).length === 0){
+    // handles null error
+    if (req.body.constructor === Object && Object.keys(req.body).length === 0){
         res.status(400).send({ error:true, message: 'Please provide all required field' });
     }else{
-        City.create(new_employee, function(err, employee) {
+        City.create(newEmployee, function(err, employee) {
             if (err)
                 res.send(err);
             res.json({error:false,message:"Employee added successfully!",data:employee});
