b63cec7da330ba252a3ce8761c96de24d829811b
qnguy <qtn3@njit.edu>
2021-04-22 22:27:32 -0500

    Fixed function expression arrow, extra space in if - else statement

--- server/controllers/cities.controllers.js
+++ server/controllers/cities.controllers.js
@@ -30,18 +30,15 @@ exports.findById = (req, res) => {
     });
 };
 
-
-exports.update = function(req, res) {
-    if(req.body.constructor === Object && Object.keys(req.body).length === 0){
-        res.status(400).send({ error:true, message: 'Please provide all required field' });
-    }else{
-        City.update(req.params.id, new City(req.body), function(err, employee) {
-            if (err)
-                res.send(err);
-            res.json({ error:false, message: 'Employee successfully updated' });
+exports.update = (req, res) => {
+    if (req.body.constructor === Object && Object.keys(req.body).length === 0) {
+        res.status(400).send({ error: true, message: 'Please provide all required field' });
+    } else {
+        City.update(req.params.id, new City(req.body), (err) => {
+            if (err) res.send(err);
+            res.json({ error: false, message: 'Employee successfully updated' });
         });
     }
-
 };
 
 
