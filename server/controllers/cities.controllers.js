10a20c9e26242771167491e43a5dc3c3a73a202e
qnguy <qtn3@njit.edu>
2021-04-22 22:24:26 -0500

    Fixed function expression arrow, missing space, and missing single quotes

--- server/controllers/cities.controllers.js
+++ server/controllers/cities.controllers.js
@@ -14,12 +14,11 @@ exports.create = (req, res) => {
 
     // handles null error
     if (req.body.constructor === Object && Object.keys(req.body).length === 0){
-        res.status(400).send({ error:true, message: 'Please provide all required field' });
-    }else{
-        City.create(newEmployee, function(err, employee) {
-            if (err)
-                res.send(err);
-            res.json({error:false,message:"Employee added successfully!",data:employee});
+        res.status(400).send({ error: true, message: 'Please provide all required field' });
+    } else {
+        City.create(newEmployee, (err, employee) => {
+            if (err) res.send(err);
+            res.json({ error: false, message: 'Employee added successfully!',data: employee });
         });
     }
 };
